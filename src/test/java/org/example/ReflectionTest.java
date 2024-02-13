2019d733b8d29f5b13e875082aed85c747c05bff
RobertRyu <ckue21@gmail.com>
2024-02-13 13:55:41 +0900

    Class heap load type 3 add

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -15,6 +15,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @Controller 어노테이션이 설정돼 있는 모든 클래스를 찾아서 출력한다.
  */
@@ -35,6 +37,29 @@ public class ReflectionTest {
         logger.debug("user all declared fields : [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
         logger.debug("user all declared constructors : [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
         logger.debug("user all declared methods : [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+    }
+
+    @Test
+    void load() throws ClassNotFoundException {
+        //1
+        Class<User> clazz = User.class;
+
+        //2
+        User user = new User("RobertRyu", "류창규");
+        Class<? extends User> clazz2 = user.getClass();
+
+        //3
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz : [{}]", clazz);
+        logger.debug("clazz2 : [{}]", clazz2);
+        logger.debug("clazz3 : [{}]", clazz3);
+
+        assertThat(clazz == clazz2).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
+        assertThat(clazz3 == clazz).isTrue();
+
+
     }
 
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
